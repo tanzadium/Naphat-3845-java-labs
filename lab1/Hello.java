a1fdef4bc4f4ba37b535a81159f4ec75230540dc
naphat.phad@kkumail.com <naphat.phad@kkumail.com>
2024-11-18 15:24:44 +0700

    branch

---
+++ lab1/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Naphat Phadungkit, and student ID is 673040384-5");
+    }
+}
--- lab1/Naphat.java
+++
@@ -1,5 +0,0 @@
-public class Naphat {
-    public static void main(String[] args){
-        System.out.println("Hello, my name is Naphat Phadungkit, and student id is 673040384-5");
-    }
-}
