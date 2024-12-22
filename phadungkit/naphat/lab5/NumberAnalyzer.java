2c5e762bd8688a6b5aa29b8bb7d465ee4ee95cc6
tanzadium <naphat.phad@kkumail.com>
2024-12-22 19:29:44 +0700

    Lab5 edit no.2

--- phadungkit/naphat/lab5/NumberAnalyzer.java
+++ phadungkit/naphat/lab5/NumberAnalyzer.java
@@ -10,8 +10,8 @@ import java.util.Arrays;
  */
 public class NumberAnalyzer {
 
-    public static int[] findMinMax(int[] testingArray) {
-        int[] arrayFindMinMax = testingArray.clone();
+    public static int[] findMinMax(int[] numbers) {
+        int[] arrayFindMinMax = numbers.clone();
         Arrays.sort(arrayFindMinMax);
         int min = arrayFindMinMax[0];
         int max = arrayFindMinMax[arrayFindMinMax.length - 1];
@@ -19,10 +19,10 @@ public class NumberAnalyzer {
         return new int[]{min, max};
     }
 
-    public static double[] calculateRunningAverages(int[] testingArray) {
+    public static double[] calculateRunningAverages(int[] numbers) {
         double currentSum = 0;
-        double[] averages = new double[testingArray.length];
-        int[] findAvg = testingArray.clone();
+        double[] averages = new double[numbers.length];
+        int[] findAvg = numbers.clone();
         double[] doublesTestingArray = Arrays.stream(findAvg).asDoubleStream().toArray();
         for (int i = 0; i < doublesTestingArray.length; i++) {
             currentSum += doublesTestingArray[i];
@@ -34,14 +34,11 @@ public class NumberAnalyzer {
 
     }
 
-    public static void printArrayWithCurlyBrace(int[] testingArray) {
-        System.out.print("{");
-        for (int i = 0; i < testingArray.length - 1; i++) {
-            System.out.print(testingArray[i] + ", ");
-        }
-        for (int i = testingArray.length - 2; i < testingArray.length - 1; i++) {
-            System.out.println(testingArray[testingArray.length - 1] + "}");
-        }
+    public static String printArrayWithCurlyBrace(int[] testingArray) {
+        String withCurly = Arrays.toString(testingArray);
+        withCurly = "{" + withCurly.substring(1, withCurly.length() - 1) + "}";
+        System.out.println(withCurly);
+        return withCurly;
     }
 
     public static void printArray(int[] testingArray) {
@@ -50,36 +47,29 @@ public class NumberAnalyzer {
         }
     }
 
-    public static void printArray2(int[] testingArray2) {
-        for (int i = 0; i < testingArray2.length; i++) {
-            System.out.print(testingArray2[i] + " ");
-        }
-    }
-
-    public static boolean isSorted(int[] testingArray) {
-        int[] unSort = testingArray.clone();
+    public static boolean isSorted(int[] numbers) {
+        int[] unSort = numbers.clone();
         Arrays.sort(unSort);
-        return Arrays.equals(unSort, testingArray);
+        return Arrays.equals(unSort, numbers);
     }
 
     public static void main(String[] args) {
         int[] testingArray = {4, 2, 7, 1, 9};
         int[] testingArray2 = {1, 2, 3, 4, 5};
+        int[] result = findMinMax(testingArray);
         System.out.print("Testing with array: ");
         printArrayWithCurlyBrace(testingArray);
-
-        int[] result = findMinMax(testingArray);
-        System.out.println("\nMinimum value: " + result[0]);
+        System.out.println("Minimum value: " + result[0]);
         System.out.println("Maximum value: " + result[1] + "\n");
 
         System.out.println("Running averages:");
         calculateRunningAverages(testingArray);
 
-        System.out.println("\nTesting if arrays is sorted:");
+        System.out.println("\nTesting if arrays are sorted:");
         printArray(testingArray);
 
         System.out.print("\nnumbers1 is sorted: " + isSorted(testingArray) + "\n");
-        printArray2(testingArray2);
+        printArray(testingArray2);
         System.out.print("\nnumbers2 is sorted: " + isSorted(testingArray2));
 
     }
