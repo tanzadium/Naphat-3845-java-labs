f0671faebd383f2dbf862d3e30a362562cd8801a
tanzadium <naphat.phad@kkumail.com>
2025-02-10 15:21:23 +0700

    Lab9 Final2

--- phadungkit/naphat/lab9/MobileDeviceV7.java
+++ phadungkit/naphat/lab9/MobileDeviceV7.java
@@ -8,8 +8,10 @@ import java.awt.*;
  * introduces a panel for reading and displaying an image of a mobile device,
  * enhancing the form with a visual representation of the selected device.
  *
- * Author: Naphat Phadungkit Student ID: 673040384-5 Sec: 1 Last updated date: 9
- * February 2025
+ * Author: Naphat Phadungkit 
+ * Student ID: 673040384-5 
+ * Sec: 1 
+ * Last updated date: 10 February 2025
  */
 public class MobileDeviceV7 extends MobileDeviceV6 {
 
@@ -76,36 +78,4 @@ public class MobileDeviceV7 extends MobileDeviceV6 {
         setFormData();
     }
 
-}
-
-/**
- * Custom JPanel class for displaying a mobile device image. Loads and renders
- * an image centered within the panel.
- */
-class ReadImage extends JPanel {
-
-    protected Image deviceImage;
-
-    // Constructs the image panel and Loads the device image from resources and sets a white background.
-    public ReadImage() {
-        ImageIcon icon = new ImageIcon(getClass().getClassLoader()
-                .getResource("phadungkit/naphat/lab9/images/S25-ultra.jpg"));
-        deviceImage = icon.getImage();
-        setBackground(Color.WHITE);
-    }
-
-    // Overrides paintComponent to custom render the image. Scales and centers the image within the panel.
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (deviceImage != null) {
-            int width = (int) (getWidth() * 0.8);
-            int height = width * deviceImage.getHeight(this) / deviceImage.getWidth(this);
-
-            int x = (getWidth() - width) / 2;
-            int y = (getHeight() - height) / 2;
-
-            g.drawImage(deviceImage, x, y, width, height, this);
-        }
-    }
 }
